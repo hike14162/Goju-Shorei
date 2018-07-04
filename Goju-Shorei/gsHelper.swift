87b9914abc602eca21978896348baba13c974af9
Rob Carr <renorob@sbcglobal.net>
2018-07-03 18:53:32 -0700

    Cleanup

--- Goju-Shorei/gsHelper.swift
+++ Goju-Shorei/gsHelper.swift
@@ -23,49 +23,6 @@ open class gsHelper
         return NSDictionary(objects: [textColor, shadow, UIFont(name: "Optima", size: 26.0)!], forKeys: [NSAttributedStringKey.foregroundColor as NSCopying, NSAttributedStringKey.shadow as NSCopying, NSAttributedStringKey.font as NSCopying])
     }
 
-    open class func stringToDouble(_ stg: String) -> Double
-    {
-        var string: NSString = NSString(string: strip(OrigStg: stg))
-        string = string.replacingOccurrences(of: ",", with: "") as NSString
-        var dblVal = string.doubleValue
-        let fmtr = NumberFormatter()
-        fmtr.usesGroupingSeparator = true
-        
-        let dblVal2 = fmtr.number(from: string as String)?.doubleValue
-        if let dv = dblVal2
-        {
-            dblVal = dv
-        }
-        
-        dblVal = round(dblVal * 100)/100
-        return dblVal
-    }
-
-    open class func strip(OrigStg origStg: String) -> String
-    {
-        var newStg = origStg
-        if (origStg.count > 0)
-        {
-            var firstChar = origStg.first
-            let isNeg: Bool = (firstChar == "-")
-            
-            let dec: CharacterSet = CharacterSet.decimalDigits.inverted
-            newStg = origStg.trimmingCharacters(in: dec)
-            
-            firstChar = "0"
-            
-            if (newStg.count > 0)
-            {
-                firstChar = newStg.first
-            }
-            
-            if ((isNeg) && (firstChar != "-"))
-            {
-                newStg = "-"+newStg;
-            }
-        }
-        return newStg
-    }
 
 }
 
--- Goju-Shorei/gsState.swift
+++ Goju-Shorei/gsState.swift
@@ -9,23 +9,9 @@ open class gsState
         return _ModelSingletonSharedInstance
     }
     
-    var isTimerMode: Bool = false
-    var timerInterval: Double = 10
-    var cancelTimer: Bool = false
-
     var currentPageIndex: Int = 0
     
     init()
     {
-        if let interval = UserDefaults.standard.object(forKey: "interval_val") as? String
-        {
-            timerInterval = gsHelper.stringToDouble(interval)
-        }
-        
-        if let timerEnabled = UserDefaults.standard.object(forKey: "timer_enabled") as? Bool
-        {
-            isTimerMode = timerEnabled
-        }
-        
     }
 }
